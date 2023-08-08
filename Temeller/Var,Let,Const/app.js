a524a6197cf9a762a9df35650713e80bfe7f9ff9
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-08-08 15:53:24 +0300

    Created Let,Const

---
+++ Temeller/Var,Let,Const/app.js
@@ -0,0 +1,26 @@
+//var
+
+// var name = "Bedirhan Sığırcı";
+
+// var name = "Bedo";
+
+// console.log(name);
+
+//let
+
+// let a,b;
+
+// a=10;
+// b=20;
+
+// console.log(a+b);
+
+//Const
+
+const name = "bedirhan sığırcı";
+
+console.log(name);
+
+// name= "bedo";
+
+// console.log(name);
