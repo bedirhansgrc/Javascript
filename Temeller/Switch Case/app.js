c7d40fef8d6bf7c863297816762041bfbbcce352
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-08-09 13:53:31 +0300

    created switch case

---
+++ Temeller/Switch Case/app.js
@@ -0,0 +1,30 @@
+const process = 2;
+
+// if (process === 1) {
+//     console.log("işlem 1");
+// }
+// else if (process === 2) {
+//     console.log("işlem 2");
+// }
+// else if (process === 3) {
+//     console.log("işlem 3");
+// }
+// else {
+//     console.log("İşlem Geçersiz");
+// }
+
+switch(process){
+    case 1:
+        console.log("işlem 1");
+        break;
+    case 2:
+        console.log("işlem 2");
+        break;
+    case 3:
+        console.log("işlem 3");
+        break;
+    default :
+        console.log("Geçersiz işlem");
+
+}
+
