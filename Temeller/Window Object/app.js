6df34d57a4ada0a8b21296ac9a0ca086f84120ff
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-08-10 01:57:54 +0300

    created window object

---
+++ Temeller/Window Object/app.js
@@ -0,0 +1,37 @@
+// alert("Selam") 
+
+// const cevap = confirm("Emin misiniz ?");
+
+// console.log(cevap);
+
+// if(cevap){
+//     console.log("silin gitsin")
+// }
+// else{
+//     console.log("silmeyin")
+// }
+
+
+const cevap = prompt("2 + 2 = ? ");
+
+
+if(cevap == 4){
+    console.log("Doğru");
+}
+else{
+    console.log("yanlıs");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
