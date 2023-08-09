7bdb65ae7e115c37d36a5a6d5c02cb4c6dfd13ea
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-08-09 13:34:59 +0300

    update

---
+++ Temeller/Koşullu Durumlar/app.js
@@ -0,0 +1,74 @@
+// Karşılaştırma Operatörleri
+
+// ==
+// === Tipini de sorguluyor
+// !=
+// !==
+// >
+// <
+// >=
+// <=
+
+
+// console.log(2 == 2);
+// console.log("js" == "java");
+
+// console.log(2 == "2");
+// console.log(2 === "2");
+
+// console.log(4 > 2);
+// console.log(2 > 4);
+
+// console.log(2 != 2);
+
+// console.log(4 < 2);
+// console.log(2 < 4);
+
+// console.log(4 >= 2);
+// console.log(2 >= 2);
+
+// Mantıksal Bağlaçlar
+
+//Not Operatörü
+
+// console.log(!(2 == 2));
+
+//And Operatörü
+
+// console.log( (2 == 2) && ("Ahmet" == "Ahmet"));
+
+//Or Operatörü
+
+// console.log((4 == 2) || ("Ahmet" == "ahmet"));
+
+// const error = false;
+
+
+// if(error == true){
+//     console.log("Hata Oluştu");
+// }
+// else{
+//     console.log("Hata Oluşmadı")
+// }
+
+// const process = "1";
+// if(process == "1"){
+//     console.log("İşlem 1");
+// }
+// else if(process == "2"){
+//     console.log("işlem 2");
+// }
+// else if(process == "3"){
+//     console.log("işlem 3");
+// }
+// else if(process == "4"){
+//     console.log("işlem 4");
+// }
+// else{
+//     console.log("Geçersiz işlem");
+// }
+
+//Ternary Operator
+const number = 100;
+
+console.log(number === 100 ? "Sayı 100" : "Sayı 100 Değil");
